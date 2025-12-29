e1aac1421c541fd695b57a8f3c664ee35f560d68
biDolu <umutcrkn@gmail.com>
2025-12-30 00:07:11 +0300

    Initial Commit

---
+++ PetShop/PetShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetShopApp.swift
+//  PetShop
+//
+//  Created by Umut on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct PetShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetShopTests/PetShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetShopTests.swift
+//  PetShopTests
+//
+//  Created by Umut on 30.12.2025.
+//
+
+import Testing
+@testable import PetShop
+
+struct PetShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
